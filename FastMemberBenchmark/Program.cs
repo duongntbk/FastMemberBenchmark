8dda61e6c321fa70e377ab5024c6b46d28ad0a85
Nguyen Thai Duong <duongnt.bk@gmail.com>
2021-07-19 23:57:20 +0900

    Refactor + add README

--- FastMemberBenchmark/Program.cs
+++ FastMemberBenchmark/Program.cs
@@ -18,115 +18,111 @@ namespace FastMemberBenchmark
         protected Account _account;
         protected TypeAccessor _typeAccessor;
         protected ObjectAccessor _objectAccessor;
-        protected PropertyInfo _nameProp;
+        protected PropertyInfo _nameProp, _passwordProp, _balanceProp;
 
         [GlobalSetup]
         public void Setup()
         {
             _account = new Account();
-            _typeAccessor = TypeAccessor.Create(typeof(Account));
-            _objectAccessor = ObjectAccessor.Create(_account);
-            _nameProp = _account.GetType().GetProperty("Name", BindingFlags.Instance | BindingFlags.Public);
+            _typeAccessor = TypeAccessor.Create(typeof(Account), true);
+            _objectAccessor = ObjectAccessor.Create(_account, true);
+            _nameProp = GetProperty(nameof(Account.Name));
+            _passwordProp = GetProperty(nameof(Account.Password));
+            _balanceProp = GetProperty(nameof(Account.Balance));
         }
+
+        private PropertyInfo GetProperty(string propName) =>
+            typeof(Account).GetProperty(propName, BindingFlags.Instance | BindingFlags.Public);
     }
 
     [MemoryDiagnoser]
     public class PublicGetRunner : TestRunner
     {
         [Benchmark]
-        public string FastMember_TypeAccessor_PublicGet()
-        {
-            return _typeAccessor[_account, "Name"] as string;
-        }
+        public string FastMember_TypeAccessor_PublicGet() =>
+            _typeAccessor[_account, nameof(Account.Name)] as string;
 
         [Benchmark]
-        public string FastMember_ObjectAccessor_PublicGet()
-        {
-            return _objectAccessor["Name"] as string;
-        }
+        public string FastMember_ObjectAccessor_PublicGet() =>
+            _objectAccessor[nameof(Account.Name)] as string;
 
         [Benchmark]
-        public string Static_PublicGet()
-        {
-            return _account.Name;
-        }
+        public string Static_PublicGet() => _account.Name;
 
         [Benchmark]
-        public string Reflection_PublicGet()
-        {
-            return _nameProp.GetValue(_account) as string;
-        }
+        public string Reflection_PublicGet() => _nameProp.GetValue(_account) as string;
     }
 
     [MemoryDiagnoser]
     public class PublicSetRunner : TestRunner
     {
         [Benchmark]
-        public void FastMember_TypeAccessor_PublicSet()
-        {
-            _typeAccessor[_account, "Name"] = "Nguyen Thai Duong";
-        }
+        public void FastMember_TypeAccessor_PublicSet() =>
+            _typeAccessor[_account, nameof(Account.Name)] = "Nguyen Thai Duong";
 
         [Benchmark]
-        public void FastMember_ObjectAccessor_PublicSet()
-        {
-            _objectAccessor["Name"] = "Nguyen Thai Duong";
-        }
+        public void FastMember_ObjectAccessor_PublicSet() =>
+            _objectAccessor[nameof(Account.Name)] = "Nguyen Thai Duong";
 
         [Benchmark]
-        public void Static_PublicSet()
-        {
-            _account.Name = "Nguyen Thai Duong";
-        }
+        public void Static_PublicSet() => _account.Name = "Nguyen Thai Duong";
 
         [Benchmark]
-        public void Reflection_PublicSet()
-        {
-            _nameProp.SetValue(_account, "Nguyen Thai Duong");
-        }
+        public void Reflection_PublicSet() => _nameProp.SetValue(_account, "Nguyen Thai Duong");
     }
 
     [MemoryDiagnoser]
     public class PrivateGetRunner : TestRunner
     {
         [Benchmark]
-        public string FastMember_TypeAccessor_PrivateGet()
-        {
-            return _typeAccessor[_account, "Password"] as string;
-        }
+        public string FastMember_TypeAccessor_PrivateGet() =>
+            _typeAccessor[_account, nameof(Account.Password)] as string;
 
         [Benchmark]
-        public string FastMember_ObjectAccessor_PrivateGet()
-        {
-            return _objectAccessor["Password"] as string;
-        }
+        public string FastMember_ObjectAccessor_PrivateGet() =>
+            _objectAccessor[nameof(Account.Password)] as string;
 
         [Benchmark]
-        public string Reflection_PrivateGet()
-        {
-            return _nameProp.GetValue(_account) as string;
-        }
+        public string Reflection_PrivateGet() => _passwordProp.GetValue(_account) as string;
     }
 
     [MemoryDiagnoser]
     public class PrivateSetRunner : TestRunner
     {
         [Benchmark]
-        public void FastMember_TypeAccessor_PrivateSet()
-        {
-            _typeAccessor[_account, "Balance"] = 2_000;
-        }
+        public void FastMember_TypeAccessor_PrivateSet() =>
+            _typeAccessor[_account, nameof(Account.Balance)] = 2_000;
 
         [Benchmark]
-        public void FastMember_ObjectAccessor_PrivateSet()
-        {
-            _objectAccessor["Balance"] = 2_000;
-        }
+        public void FastMember_ObjectAccessor_PrivateSet() =>
+            _objectAccessor[nameof(Account.Balance)] = 2_000;
 
         [Benchmark]
-        public void Reflection_PrivateSet()
-        {
-            _nameProp.SetValue(_account, 2_000);
-        }
+        public void Reflection_PrivateSet() => _balanceProp.SetValue(_account, 2_000);
+    }
+
+    [MemoryDiagnoser]
+    public class AccessorCreateRunner
+    {
+        private Account _account;
+
+        [GlobalSetup]
+        public void Setup() => _account = new Account();
+
+        [Benchmark]
+        public TypeAccessor TypeAccessor_Create_DisallowNonPublic() =>
+            TypeAccessor.Create(typeof(Account));
+
+        [Benchmark]
+        public TypeAccessor TypeAccessor_Create_AllowNonPublic() =>
+            TypeAccessor.Create(typeof(Account), true);
+
+        [Benchmark]
+        public ObjectAccessor ObjectAccessor_Create_DisallowNonPublic() =>
+            ObjectAccessor.Create(_account);
+
+        [Benchmark]
+        public ObjectAccessor ObjectAccessor_Create_AllowNonPublic() =>
+            ObjectAccessor.Create(_account, true);
     }
 }
--- README.md
+++ README.md
@@ -5,7 +5,31 @@ I also wrote a blog post about FastMember, you can find it at the following link
 
 # Usage
 
-WIP
+Run the following command to start the benchmark.
+```
+dotnet run --configuration Release
+```
+
+You should then see the following list of tests
+```
+Available Benchmarks:
+  #0 AccessorCreateRunner
+  #1 PrivateGetRunner
+  #2 PrivateSetRunner
+  #3 PublicGetRunner
+  #4 PublicSetRunner
+```
+
+Then select the test you want to run by typing its name or number. It's possible to run multiple tests back to back. For example, you can run `PrivateGetRunner` and and `PrivateSetRunner` by entering this.
+```
+1 2
+```
+
+Or
+
+```
+PrivateGetRunner PrivateSetRunner
+```
 
 # License
 
